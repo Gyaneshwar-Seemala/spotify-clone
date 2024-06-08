1c016745a8ae7208e6ad06d6ba8e92efc013be32
Gyaneshwar-Seemala <johnnyseemala04@gmail.com>
2024-06-09 00:18:08 +0530

    Clone App

---
+++ src/components/SongItems.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { playerContext } from '../context/PlayerContext'
+
+const SongItems = ({name,image,desc,id}) => {
+    const {playWithID}=useContext(playerContext)
+  return (
+    <div onClick={()=>playWithID(id)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+      <img className='rounded' src={image}/>
+      <p className='font-bold mt-2 mb-1'>{name}</p>
+      <p className='text-slate-200 text-sm'>{desc}</p>
+    </div>
+  )
+}
+
+export default SongItems
